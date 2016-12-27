b6921e3c27dafb00217bdc899923bc91bbbb443a
Tobias Munk <schmunk@usrbin.de>
2016-12-27 21:38:36 +0100

    updated build config

--- src/config/web.php
+++ src/config/web.php
@@ -6,7 +6,7 @@
  * @copyright Copyright (c) 2016 diemeisterei GmbH, Stuttgart
  *
  * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * file that was distributed with this source code
  */
 
 // Settings for web-application only
