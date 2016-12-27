4705719b771aa12802a4c17c2eb0b43be5f0a39e
Tobias Munk <schmunk@usrbin.de>
2016-12-27 21:38:21 +0100

    updated demo data

---
+++ src/migrations/demo-data/m160720_000000_demo_data.php
@@ -0,0 +1,7 @@
+<?php
+
+
+class m160720_000000_demo_data extends \dmstr\db\mysql\FileMigration
+{
+    public $file = 'm161215_183747_app_0.8.2-1-ga18d13a_data.sql';
+}
---
+++ src/migrations/demo-data/m160721_000000_users_and_auth.php
@@ -0,0 +1,6 @@
+<?php
+
+
+class m160721_000000_users_and_auth extends \dmstr\db\mysql\FileMigration
+{
+}
