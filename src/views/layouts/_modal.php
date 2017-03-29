02f1eb451ce77bce206a1dec4858a49d474e4d37
Tobias Munk <schmunk@usrbin.de>
2017-03-29 22:41:07 +0200

    updated info modal

--- src/views/layouts/_modal.php
+++ src/views/layouts/_modal.php
@@ -10,32 +10,40 @@ use yii\helpers\Html;
             <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span
                     class="sr-only">Close</span></button>
 
-            <h3>
-                <?= getenv('APP_NAME') ?>
-            </h3>
-
             <p>
-                <small>
-                    <span class="label <?= YII_ENV_PROD ? 'label-success' : 'label-danger' ?>"><?= YII_ENV ?></span>
-                    <span class="label label-warning <?= YII_DEBUG ? '' : 'hidden' ?>">debug</span>
-                </small>
-
-            </p>
-            <p style="line-height: 1.6em">
-                Application Version <span class="label label-info"><?= getenv('APP_NAME') ?>-<?= APP_VERSION ?></span>
-                <br>
-                Virtual Host <span class="label label-default"><?= isset($_SERVER['HTTP_HOST']) ?
-                        $_SERVER['HTTP_HOST'] : '' ?></span>
-                <br/>
-                Hostname <span class="label label-default"><?= getenv('HOSTNAME') ?: 'local' ?></span>
+            <h3><?= getenv('APP_TITLE') ?></h3>
+            <small>
+                <span class="label <?= YII_ENV_PROD ? 'label-success' : 'label-danger' ?>"><?= YII_ENV ?></span>
+                <span class="label label-warning <?= YII_DEBUG ? '' : 'hidden' ?>">debug</span>
+            </small>
             </p>
 
-            <p class="small">
-                Built with <?= Html::a('phd5', 'http://phd.dmstr.io') ?> from <?= Html::a('dmstr',
-                    'http://www.diemeisterei.de') ?>
-            </p>
+            <table class="table">
+                <tr>
+                    <th>Name</th>
+                    <td><code><?= getenv('APP_NAME') ?></code></td>
+                </tr>
+                <tr>
+                    <th>Version</th>
+                    <td><code><?= APP_VERSION ?></code></td>
+                </tr>
+                <tr>
+                    <th>Virtual Host</th>
+                    <td><code><?= isset($_SERVER['HTTP_HOST']) ?
+                                $_SERVER['HTTP_HOST'] : 'n/a' ?></code></td>
+                </tr>
+                <tr>
+                    <th>Hostname</th>
+                    <td><code><?= getenv('HOSTNAME') ?: 'local' ?></code></td>
+                </tr>
+            </table>
 
-            <hr/>
+            <div class="pull-left">
+                <p class="small">
+                    Built with <?= Html::a('phd', 'http://phd.dmstr.io') ?> from <?= Html::a('dmstr',
+                        'http://www.diemeisterei.de') ?>
+                </p>
+            </div>
 
             <div class="pull-right">
                 <?= Html::a(FA::icon(FA::_COG), ['/backend'], ['class' => 'btn btn-default btn-xs']) ?>
