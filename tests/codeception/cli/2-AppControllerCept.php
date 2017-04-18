2fa13af1e59bbc4e89a463d1518fa14596e066ca
Tobias Munk <schmunk@usrbin.de>
2017-04-18 13:49:11 +0200

    added cleanup cli tests

--- tests/codeception/cli/2-AppControllerCept.php
+++ tests/codeception/cli/2-AppControllerCept.php
@@ -13,3 +13,9 @@ $I->seeInShellOutput('APP_NAME');
 
 $I->runShellCommand('yii app/version');
 $I->seeInShellOutput('Application Version');
+
+$I->runShellCommand('yii app/cleanup');
+$I->seeInShellOutput('Cleanup');
+$I->seeInShellOutput('Cleanup was successful.');
+$I->seeInShellOutput('Web assets have been deleted.');
+
