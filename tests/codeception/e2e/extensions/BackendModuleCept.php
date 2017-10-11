66913932bcc084f2cc4b88bc142d442015a10f86
Tobias Munk <schmunk@usrbin.de>
2017-10-11 18:17:22 +0200

    added backend test

---
+++ tests/codeception/e2e/extensions/BackendModuleCept.php
@@ -0,0 +1,15 @@
+<?php
+
+// @group mandatory
+
+$I = new E2eTester($scenario);
+$I->wantTo('ensure that Backend works');
+
+$I->amGoingTo('try to login with correct credentials');
+$I->login('admin', 'admin1');
+
+$I->amGoingTo('test backend menu');
+$I->amOnPage('/backend');
+$I->dontSee('Backend navigation');
+$I->click('.sidebar-toggle.btn');
+$I->see('Backend navigation');
