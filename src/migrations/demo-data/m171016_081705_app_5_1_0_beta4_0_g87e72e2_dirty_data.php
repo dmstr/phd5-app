1cc39c20e93dc5062339c3691a6c81fc3100a4cf
Tobias Munk <schmunk@usrbin.de>
2017-10-16 12:30:56 +0200

    fixed filenme

--- src/migrations/demo-data/m171016_081705_app_5_1_0_beta4_0_g87e72e2_dirty_data.php
+++ src/migrations/demo-data/m171016_081705_app_5_1_0_beta4_0_g87e72e2_dirty_data.php
@@ -4,5 +4,5 @@ use yii\db\Migration;
 
 class m171016_081705_app_5_1_0_beta4_0_g87e72e2_dirty_data extends \dmstr\db\mysql\FileMigration
 {
-    #public $file = 'custom_filename';
+    public $file = 'm171016_101636_app_5_1_0_beta4_0_g87e72e2_dirty_data.sql';
 }
