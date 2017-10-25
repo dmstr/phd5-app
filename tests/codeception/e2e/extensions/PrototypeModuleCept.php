dc64f0d00c47e9989c1fb572ce3d48b860b93d78
Tobias Munk <schmunk@usrbin.de>
2017-10-25 20:02:56 +0200

    hardened test

--- tests/codeception/e2e/extensions/PrototypeModuleCept.php
+++ tests/codeception/e2e/extensions/PrototypeModuleCept.php
@@ -12,10 +12,13 @@ $I->amGoingTo('try to view and create snippets');
 $I->amOnPage('/prototype');
 
 $I->amOnPage('/prototype/less/create');
+$I->waitForElementVisible('.giiant-crud button[type="submit"]');
 $I->canSee('Create', 'button');
 
 $I->amOnPage('/prototype/twig/create');
+$I->waitForElementVisible('.giiant-crud button[type="submit"]');
 $I->canSee('Create', 'button');
 
 $I->amOnPage('/prototype/html/create');
+$I->waitForElementVisible('.giiant-crud button[type="submit"]');
 $I->canSee('Create', 'button');
