a72e04c97715a9fb9cb99cf936d490bb53dec606
Tobias Munk <schmunk@usrbin.de>
2017-10-25 18:51:50 +0200

    updated demo-data

--- src/migrations/demo-data/m171016_081705_app_5_1_0_beta4_0_g87e72e2_dirty_data.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use yii\db\Migration;
-
-class m171016_081705_app_5_1_0_beta4_0_g87e72e2_dirty_data extends \dmstr\db\mysql\FileMigration
-{
-    public $file = 'm171016_101636_app_5_1_0_beta4_0_g87e72e2_dirty_data.sql';
-}
---
+++ src/migrations/demo-data/m171025_163829_app_5_1_0_beta8_2_ga636402_data.php
@@ -0,0 +1,8 @@
+<?php
+
+use yii\db\Migration;
+
+class m171025_163829_app_5_1_0_beta8_2_ga636402_data extends \dmstr\db\mysql\FileMigration
+{
+    #public $file = '';
+}
