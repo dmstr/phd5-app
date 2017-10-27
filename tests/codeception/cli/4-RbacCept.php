3857ed0fc1e3a0e2d989a56b45c7f3dbdcc43d5a
Tobias Munk <schmunk@usrbin.de>
2017-10-27 18:57:14 +0200

    updated packages; fixed RBAC command

--- tests/codeception/cli/4-RbacCept.php
+++ tests/codeception/cli/4-RbacCept.php
@@ -11,4 +11,8 @@ $I->amGoingTo('run the rbac command');
 $I->runShellCommand("yii user/create {$userName}@test.local {$userName} test1234");
 $I->runShellCommand("yii rbac/assign Editor {$userName}");
 $I->canSeeInShellOutput('Role has been assigned');
+
+$I->runShellCommand("yii rbac/assign Editor {$userName}");
+$I->canSeeInShellOutput('Role is already assigned to this user');
+
 $I->seeInShellOutput('Done.');
