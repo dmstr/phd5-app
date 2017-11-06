2fb847fa2ab559b86fa9705da877f66c76c36a90
Tobias Munk <schmunk@usrbin.de>
2017-11-06 10:00:07 +0100

    removed translatable title (custom titles are handled via settings)

--- src/views/site/index.php
+++ src/views/site/index.php
@@ -1,8 +1,3 @@
-<?php
-/* @var $this yii\web\View */
-$this->title .= Yii::t('app', '__TITLE_HOME__');
-?>
-
 <div class="site-index">
 
     <?= \dmstr\modules\prototype\widgets\TwigWidget::widget(['registerMenuItems' => true]) ?>
