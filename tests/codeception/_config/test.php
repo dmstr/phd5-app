2bc3b657327ce7992c4ba19c8f5e959eb451cd6c
schmunk42 <schmunk@usrbin.de>
2018-04-16 20:33:34 +0200

    added response check in tests

--- tests/codeception/_config/test.php
+++ tests/codeception/_config/test.php
@@ -11,9 +11,20 @@
 $_SERVER['HOST_NAME'] = 'web';
 $_SERVER['REQUEST_TIME'] = time();
 
+// check response code before rendering the page, if it is a redirect something went wrong
+\yii\base\Event::on(
+    \yii\web\View::class,
+    \yii\web\View::EVENT_BEFORE_RENDER,
+    function () {
+        if (Yii::$app instanceof \yii\web\Application && Yii::$app->response->statusCode >= 300 && Yii::$app->response->statusCode < 400) {
+            throw new \yii\web\HttpException(500, 'Unexpected response status code');
+        }
+    }
+);
+
 return [
     'language' => 'en',
     'params' => [
         'backend.iframe.name' => 'backend-test',
-    ]
+    ],
 ];
