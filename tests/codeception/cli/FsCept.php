6ecf4b12212ee67c938e7204dd3507cd4a4d8392
Tobias Munk <schmunk@usrbin.de>
2018-05-25 18:48:19 +0200

    hardened filesystem test

--- tests/codeception/cli/FsCept.php
+++ tests/codeception/cli/FsCept.php
@@ -2,9 +2,17 @@
 
 // @group mandatory
 
+$dir = \Codeception\Util\Fixtures::get('uniqid');
+
 $I = new CliTester($scenario);
 
 $I->runShellCommand('yii fs');
 $I->seeInShellOutput('FlyCLI');
 
+$I->runShellCommand('yii fs/mkdir local://'.$dir);
+
 $I->runShellCommand('yii fs/ls local://');
+$I->seeInShellOutput($dir);
+
+$I->runShellCommand('yii fs/rmdir local://'.$dir);
+$I->dontSeeInShellOutput($dir);
