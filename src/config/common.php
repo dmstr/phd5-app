2e90b62de7e930662ab777d37f323219f56a3e2b
Tobias Munk <schmunk@usrbin.de>
2018-07-02 08:04:23 +0200

    created signed imageproxy request, if APP_IMAGEPROXY_KEY is set

--- src/config/common.php
+++ src/config/common.php
@@ -200,6 +200,10 @@ return [
                     'functions' => [
                         'image' => function ($imageSource, $preset = null) {
                             // preset example, when using imageproxy, https://github.com/willnorris/imageproxy#examples
+                            if (getenv('APP_IMAGEPROXY_KEY')) {
+                                $key = getenv('APP_IMAGEPROXY_KEY');
+                                $preset .= ',s' . strtr(base64_encode(hash_hmac('sha256', $imageSource, $key, 1)), '/+' , '_-');
+                            }
                             return Yii::$app->settings->get('imgBaseUrl', 'app.frontend') .
                                 $preset .
                                 Yii::$app->settings->get('imgHostPrefix', 'app.frontend') .
