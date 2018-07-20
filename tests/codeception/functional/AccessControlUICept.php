5cea3f28bcebf72e05e58921d6033330f4f4af3a
Elias Luhr <e.luhr@herzogkommunikation.de>
2018-07-20 09:10:45 +0200

    fixed tests

--- tests/codeception/functional/AccessControlUICept.php
+++ tests/codeception/functional/AccessControlUICept.php
@@ -2,13 +2,13 @@
 
 // @group mandatory
 
-use tests\codeception\_pages\LoginPage;
+//use tests\codeception\_pages\LoginPage;
 
 $I = new FunctionalTester($scenario);
 $I->wantTo('ensure that access control UI works');
 
 $I->amOnPage('/');
-$I->dontSeeLink('/de/backend', '.nav');
+$I->dontSeeLink('/en/backend', '.nav');
 $I->dontSee('','.glyphicon.glyphicon-cog');
 
 $I->login('admin', 'admin1');
