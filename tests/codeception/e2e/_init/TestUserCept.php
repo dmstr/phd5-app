ef51ee742cc17670e87d24db9cd6b99dfd4c697e
Elias Luhr <e.luhr@herzogkommunikation.de>
2018-07-20 09:33:34 +0200

    fixed test -> usuario uses selectize instead of select2

--- tests/codeception/e2e/_init/TestUserCept.php
+++ tests/codeception/e2e/_init/TestUserCept.php
@@ -33,7 +33,8 @@ $I->waitForElementVisible('[data-value="pages"]', 10);
 $I->click('[data-value="pages"]');
 $I->waitForElementVisible('.item[data-value="pages"]', 10);
 $I->makeScreenshot('success-preview-items');
-
+$I->click('Create'); // hide selectize drop-down
+$I->makeScreenshot('success-preview-items-remove');
 $I->click('Update assignments');
 $I->wait(2);
 $I->makeScreenshot('success-preview-items-update');
