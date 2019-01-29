94fb1684525cf5539fd5394a5ff53e4290563e58
Tobias Munk <schmunk@usrbin.de>
2019-01-29 14:54:18 +0100

    added db scanner

--- tests/codeception/cli/TranslateCept.php
+++ tests/codeception/cli/TranslateCept.php
@@ -7,6 +7,7 @@ $I = new CliTester($scenario);
 $I->runShellCommand('yii translate/scan');
 $I->seeInShellOutput('Detect PhpFunction - BEGIN');
 $I->seeShellOutputMatches('/Detected language element(.)/');
+$I->seeShellOutputMatches('/Extracting messages from(.*)twig_template/');
 
 $I->runShellCommand('yii translate/optimize');
 $I->seeInShellOutput('removed from database.');
