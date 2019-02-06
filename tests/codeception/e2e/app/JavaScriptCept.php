865fae45f32d7671ffae5dab1d25bc405b379332
Tobias Munk <schmunk@usrbin.de>
2019-02-06 13:16:11 +0100

    updated JavaScript test, modal requires login

--- tests/codeception/e2e/app/JavaScriptCept.php
+++ tests/codeception/e2e/app/JavaScriptCept.php
@@ -11,6 +11,8 @@ $I->amGoingTo('check javascript with a modal');
 $I->amOnPage('/en');
 $I->dontSee('#phd-info-panel');
 
+$I->login('editor', 'editor1');
+
 $I->amGoingTo('display the info button');
 $I->pressKey("body", "h");
 $I->click('#phd-info-button a');
