db55a10ebd28da653c94a55da43834f7bbb9c0cc
Elias Luhr <e.luhr@herzogkommunikation.de>
2021-04-21 11:22:20 +0200

    improved alias

--- config/common.php
+++ config/common.php
@@ -163,7 +163,7 @@ return [
             'hashCallback' => getenv('APP_ASSET_FORCE_PUBLISH') ? AssetHash::byFileTime(!YII_DEBUG) : null,
             // Note: You need to bundle asset with `yii asset` for development/debugging
             'bundles' => $bundles,
-            'basePath' => '@app/../web/assets/'
+            'basePath' => '@web/assets/'
         ],
         'cache' => [
             'class' => getenv('APP_NO_CACHE') ? DummyCache::class : Cache::class
