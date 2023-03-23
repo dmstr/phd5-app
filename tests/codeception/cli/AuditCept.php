e837df706121716569f74d770aa5e46029c698bf
Tobias Munk <schmunk@usrbin.de>
2023-03-23 13:52:56 +0100

    updated CLI audit tests

--- tests/codeception/cli/AuditCept.php
+++ tests/codeception/cli/AuditCept.php
@@ -6,5 +6,6 @@ $I = new CliTester($scenario);
 
 $I->runShellCommand('yii audit/cleanup --interactive=0');
 $I->seeInShellOutput('skipped audit/trail');
-$I->seeInShellOutput('skipped audit/mail');
+$I->seeInShellOutput('cleaned audit/mail');
+$I->seeInShellOutput('cleaned audit/error');
 $I->seeInShellOutput('Cleanup was successful');
