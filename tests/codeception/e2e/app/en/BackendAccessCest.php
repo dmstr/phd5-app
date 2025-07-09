0bf412fd271dcadccb9cbd8d65bd81e6859fcb80
Elias Luhr <e.luhr@herzogkommunikation.de>
2025-07-09 14:17:14 +0200

    new resque modules does not have title in layout so updated test

--- tests/codeception/e2e/app/en/BackendAccessCest.php
+++ tests/codeception/e2e/app/en/BackendAccessCest.php
@@ -48,7 +48,6 @@ class BackendAccessCest
         $I->makeScreenshot('user-admin');
 
         $I->amOnPage('/resque/manage');
-        $I->see('Workers', '.small-box');
         $I->see('Waiting');
         $I->see('Reserved');
         $I->see('Delayed');
