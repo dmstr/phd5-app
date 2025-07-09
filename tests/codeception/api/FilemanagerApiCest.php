ca4e0bdd05f0a01f0865771f20949d99e33a326b
jvb <j.vonbradke@herzogkommunikation.de>
2025-07-09 15:35:20 +0200

    Add Directory Tests

--- tests/codeception/api/FilemanagerApiCest.php
+++ tests/codeception/api/FilemanagerApiCest.php
@@ -206,8 +206,295 @@ final class FilemanagerApiCest
             'path' => '/',
             'name' => $name
         ]);
-        $I->seeResponseCodeIs(HttpCode::CREATED);
+        $I->seeResponseCodeIs(HttpCode::OK);
         $I->seeResponseIsJson();
         $I->seeResponseContainsJson(['success' => true, 'message' => 'created']);
     }
+
+    public function testCreateDirInDir(ApiTester $I)
+    {
+        $faker = Factory::create();
+        $parentDir = 'parent-' . uniqid();
+        $childDir = 'child-' . uniqid();
+
+        $I->sendPOST('/filemanager/api/create-directory', [
+            'path' => '/',
+            'name' => $parentDir
+        ]);
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson(['success' => true, 'message' => 'created']);
+
+        $I->sendPOST('/filemanager/api/create-directory', [
+            'path' => '/' . $parentDir,
+            'name' => $childDir
+        ]);
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson(['success' => true, 'message' => 'created']);
+
+        $I->sendGET('/filemanager/api/list', ['path' => '/' . $parentDir]);
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson(['name' => $childDir]);
+    }
+    public function testCreateThenDeleteDirInDir(ApiTester $I)
+    {
+        $parentDir = 'parent-' . uniqid();
+        $childDir = 'child-' . uniqid();
+
+        $I->sendPOST('/filemanager/api/create-directory', [
+            'path' => '/',
+            'name' => $parentDir
+        ]);
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson(['success' => true, 'message' => 'created']);
+
+        $I->sendPOST('/filemanager/api/create-directory', [
+            'path' => '/' . $parentDir,
+            'name' => $childDir
+        ]);
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson(['success' => true, 'message' => 'created']);
+
+        $I->sendGET('/filemanager/api/list', ['path' => '/' . $parentDir]);
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson(['name' => $childDir]);
+
+        $I->sendDELETE('/filemanager/api/delete-directory?path=/' . $parentDir . '/' . $childDir);
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson(['success' => true]);
+
+        $I->sendGET('/filemanager/api/list', ['path' => '/' . $parentDir]);
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseIsJson();
+        $I->dontSeeResponseContainsJson(['name' => $childDir]);
+    }
+    public function testCreateThenDeleteFirstDirInDir(ApiTester $I)
+    {
+        $parentDir = 'parent-' . uniqid();
+        $childDir = 'child-' . uniqid();
+
+        $I->sendPOST('/filemanager/api/create-directory', [
+            'path' => '/',
+            'name' => $parentDir
+        ]);
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson(['success' => true, 'message' => 'created']);
+
+        $I->sendPOST('/filemanager/api/create-directory', [
+            'path' => '/' . $parentDir,
+            'name' => $childDir
+        ]);
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson(['success' => true, 'message' => 'created']);
+
+        $I->sendGET('/filemanager/api/list', ['path' => '/' . $parentDir]);
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson(['name' => $childDir]);
+
+        $I->sendDELETE('/filemanager/api/delete-directory?path=/' . $parentDir);
+        $I->seeResponseCodeIs(HttpCode::UNPROCESSABLE_ENTITY);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson(['message' => 'Directory is not empty and can not be deleted']);
+
+        $I->sendGET('/filemanager/api/list', ['path' => '/']);
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson(['name' => $parentDir]);
+    }
+    public function testDeleteDotDirInDir(ApiTester $I)
+    {
+        $parentDir = 'parent-' . uniqid();
+
+        $I->sendPOST('/filemanager/api/create-directory', [
+            'path' => '/',
+            'name' => $parentDir
+        ]);
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson(['success' => true, 'message' => 'created']);
+
+        $I->sendDELETE('/filemanager/api/delete-directory?path=/' . $parentDir . '/.');
+        $I->seeResponseCodeIs(HttpCode::BAD_REQUEST);
+        $I->seeResponseIsJson();
+
+        $I->sendDELETE('/filemanager/api/delete-directory?path=/' . $parentDir . '/..');
+        $I->seeResponseCodeIs(HttpCode::BAD_REQUEST);
+        $I->seeResponseIsJson();
+
+        $I->sendGET('/filemanager/api/list', ['path' => '/']);
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson(['name' => $parentDir]);
+    }
+
+    public function testCreateDirectoryWithSpecialCharacters(ApiTester $I)
+    {
+        $specialDirs = [
+            'test-with-dash-' . uniqid(),
+            'test_with_underscore_' . uniqid(),
+            'test with space ' . uniqid(),
+            'test.with.dots.' . uniqid()
+        ];
+
+        foreach ($specialDirs as $dirName) {
+            $I->sendPOST('/filemanager/api/create-directory', [
+                'path' => '/',
+                'name' => $dirName
+            ]);
+            $I->seeResponseCodeIs(HttpCode::OK);
+            $I->seeResponseIsJson();
+            $I->seeResponseContainsJson(['success' => true, 'message' => 'created']);
+
+            $I->sendGET('/filemanager/api/list', ['path' => '/']);
+            $I->seeResponseCodeIs(HttpCode::OK);
+            $I->seeResponseIsJson();
+            $I->seeResponseContainsJson(['name' => $dirName]);
+        }
+    }
+
+    public function testCreateDirectoryWithLongName(ApiTester $I)
+    {
+        $longDirName = str_repeat('verylongdirectoryname', 10) . uniqid();
+        
+        $I->sendPOST('/filemanager/api/create-directory', [
+            'path' => '/',
+            'name' => $longDirName
+        ]);
+        
+        $I->seeResponseIsJson();
+    }
+
+    public function testCreateDuplicateDirectory(ApiTester $I)
+    {
+        $dirName = 'duplicate-test-' . uniqid();
+
+        $I->sendPOST('/filemanager/api/create-directory', [
+            'path' => '/',
+            'name' => $dirName
+        ]);
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson(['success' => true, 'message' => 'created']);
+
+        $I->sendPOST('/filemanager/api/create-directory', [
+            'path' => '/',
+            'name' => $dirName
+        ]);
+        $I->seeResponseCodeIs(HttpCode::INTERNAL_SERVER_ERROR);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson(['success' => false]);
+    }
+
+    public function testCreateDirectoryWithInvalidPath(ApiTester $I)
+    {
+        $dirName = 'invalid-path-test-' . uniqid();
+
+        $I->sendPOST('/filemanager/api/create-directory', [
+            'path' => '/nonexistent/path',
+            'name' => $dirName
+        ]);
+        $I->seeResponseCodeIs(HttpCode::NOT_FOUND);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson(['success' => false]);
+    }
+
+    public function testListDirectoryContents(ApiTester $I)
+    {
+        $parentDir = 'list-test-parent-' . uniqid();
+        $childDir1 = 'child1-' . uniqid();
+        $childDir2 = 'child2-' . uniqid();
+
+        $I->sendPOST('/filemanager/api/create-directory', [
+            'path' => '/',
+            'name' => $parentDir
+        ]);
+        $I->seeResponseCodeIs(HttpCode::OK);
+
+        $I->sendPOST('/filemanager/api/create-directory', [
+            'path' => '/' . $parentDir,
+            'name' => $childDir1
+        ]);
+        $I->seeResponseCodeIs(HttpCode::OK);
+
+        $I->sendPOST('/filemanager/api/create-directory', [
+            'path' => '/' . $parentDir,
+            'name' => $childDir2
+        ]);
+        $I->seeResponseCodeIs(HttpCode::OK);
+
+        $I->sendGET('/filemanager/api/list', ['path' => '/' . $parentDir]);
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson(['name' => $childDir1]);
+        $I->seeResponseContainsJson(['name' => $childDir2]);
+    }
+
+    public function testDeepNestedDirectoryOperations(ApiTester $I)
+    {
+        $level1 = 'level1-' . uniqid();
+        $level2 = 'level2-' . uniqid();
+        $level3 = 'level3-' . uniqid();
+
+        $I->sendPOST('/filemanager/api/create-directory', [
+            'path' => '/',
+            'name' => $level1
+        ]);
+        $I->seeResponseCodeIs(HttpCode::OK);
+
+        $I->sendPOST('/filemanager/api/create-directory', [
+            'path' => '/' . $level1,
+            'name' => $level2
+        ]);
+        $I->seeResponseCodeIs(HttpCode::OK);
+
+        $I->sendPOST('/filemanager/api/create-directory', [
+            'path' => '/' . $level1 . '/' . $level2,
+            'name' => $level3
+        ]);
+        $I->seeResponseCodeIs(HttpCode::OK);
+
+        $I->sendGET('/filemanager/api/list', ['path' => '/' . $level1 . '/' . $level2]);
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson(['name' => $level3]);
+
+        $I->sendDELETE('/filemanager/api/delete-directory?path=/' . $level1 . '/' . $level2 . '/' . $level3);
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson(['success' => true]);
+
+        $I->sendGET('/filemanager/api/list', ['path' => '/' . $level1 . '/' . $level2]);
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseIsJson();
+        $I->dontSeeResponseContainsJson(['name' => $level3]);
+    }
+
+    public function testDirectoryPermissions(ApiTester $I)
+    {
+        $testDir = 'permissions-test-' . uniqid();
+
+        $I->sendPOST('/filemanager/api/create-directory', [
+            'path' => '/',
+            'name' => $testDir
+        ]);
+        $I->seeResponseCodeIs(HttpCode::OK);
+
+        $I->sendGET('/filemanager/api/list', ['path' => '/' . $testDir]);
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson(['guiPermissions' => ['read' => true]]);
+        $I->seeResponseContainsJson(['guiPermissions' => ['write' => true]]);
+        $I->seeResponseContainsJson(['guiPermissions' => ['delete' => true]]);
+        $I->seeResponseContainsJson(['guiPermissions' => ['canChangePermissions' => true]]);
+    }
+
 }
